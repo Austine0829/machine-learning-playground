3515f8b5a2e1b46bb5edce1ad42bc8ae9aa69807
Austine0829 <audreytuazon29@gmail.com>
2025-11-01 23:38:24 +0800

    (add) loops

---
+++ python-fundamentals/loops.py
@@ -0,0 +1,18 @@
+vector = [1, 2, 3]
+
+# for feature in vector:
+#     print(feature, end=" ")
+
+# for i in range(len(vector)):
+#     print(feature[i], end=" ")
+
+# i = 0
+
+# while i != 3:
+#     print(vector[i], end=" ")
+#     i += 1
+
+for i in range (len(vector) - 1, -1, -1):
+    print(vector[i], end=" ")
+
+print()
