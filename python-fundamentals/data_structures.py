fd1925da2c14d3d401ab9c8f940cbd7fe065cdc8
Austine0829 <audreytuazon29@gmail.com>
2025-11-01 23:37:29 +0800

    (add) ds samples

---
+++ python-fundamentals/data_structures.py
@@ -0,0 +1,22 @@
+# from collections import deque
+
+# stack = deque()
+
+# stack.append(20)
+# stack.append(30)
+# stack.append(40)
+# stack.appendleft(50)
+
+# stack.popleft()
+
+# print(stack)
+
+stack = []
+
+stack.append(20)
+stack.append(30)
+stack.append(40)
+
+stack.pop()
+
+print(stack)
