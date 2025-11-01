7ff415b11c28c21c0d6b4fc11af47819156c9cf8
Austine0829 <audreytuazon29@gmail.com>
2025-11-01 23:57:36 +0800

    (add) conditionals

---
+++ python-fundamentals/conditionals.py
@@ -0,0 +1,18 @@
+grade = 92
+
+# if grade >= 90:
+#     print("Excellent")
+# else:
+#     print("Very Good")
+
+
+# if grade == 90: 
+#     print("Excelento")
+# else: 
+#     print("Still Excelento")
+
+# print(grade != 90)
+
+result = "True ya" if grade > 90 else "False ya"
+
+print(result)
