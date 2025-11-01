a9a495456c013822e5db19d8e3900819f9aaa8c4
Austine0829 <audreytuazon29@gmail.com>
2025-11-01 17:43:06 +0800

    (add) py fundamentals folder

---
+++ python-fundamentals/variables_x_collections.py
@@ -0,0 +1,18 @@
+# _string = "Austine"
+# _float = 2.50
+# _int = 1
+# _boolean = False
+
+# print(_int)
+
+
+list = [1, 2, 3, 4]
+list[0] = 10
+
+tuple = (1, 2)
+
+set = {1, 2, 3, 3}
+
+dictionary = {"name": "Austine", "age": 25}
+
+print(dictionary["age"])
