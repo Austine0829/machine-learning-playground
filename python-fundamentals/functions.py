20dc1cd9d902c5289ae394ed3b5de40c62b39a21
Austine0829 <audreytuazon29@gmail.com>
2025-11-02 13:56:04 +0800

    (add) function samples

---
+++ python-fundamentals/functions.py
@@ -0,0 +1,66 @@
+# def Addition(x, y):
+    # return x + y
+# 
+# 
+# result = Addition(10, 20)
+# 
+# print(result)
+
+
+
+# def SubractxMultiply(x, y):
+#     return (x - y), (x * y)
+
+
+# x = 10
+# y = 20
+# subtract, multiply = SubractxMultiply(x, y)
+
+
+# print("Subraction Result:", subtract)
+# print("Multiplication Result:", multiply)
+
+
+
+# def show(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
+
+# show(1, 2, 3, firstName="Austine", lastName="Tuazon")
+
+
+
+# addition = lambda x, y: x + y
+
+# print(addition(10, 20))
+
+
+
+# name = input("Enter Your Name: ")
+
+# print("Your Name:", name)
+
+vector = [1, 1, 2, 3]
+abc = list("ABC")
+oneTwoThree = str(123)
+vectorToSet = set(vector)
+vectorToTuple = tuple(vector)
+
+tuple = tuple([1, 2, 3])
+
+sum = sum(tuple)
+max = max(tuple)
+min = min(tuple)
+round = round(1.16, 1)
+squared = pow(4, 2)
+
+data = [3, 2, 1]
+sort = sorted(data)
+
+# for i in range(len(data)):
+#     print(f"Index[{i}]: {data[i]}")
+
+for i, j in enumerate(data):
+    print(i, j)
+
+print()
